e6c44d5a2dc37d5e8c16a318c98d3386428bf521
govandpik <69141721+govandpik@users.noreply.github.com>
2020-09-20 03:02:24 +0300

    Update naqeb.js

--- naqeb.js
+++ naqeb.js
@@ -536,7 +536,7 @@ naqeb.on("message", async message => {
       .setColor("#FF0000")
       .setTimestamp()
       .setFooter("Sponsored By: N1 SQUAD")
-      .setAuthor(`https://cdn.discordapp.com/icons/584491260484648962/a_c12dee5ef013c83f4556666cb864b465.gif?size=1024`)
+      
       .setThumbnail(message.author.avatarURL).setDescription(`
       
                 **The prefix for the bot is**: t!
@@ -625,7 +625,7 @@ naqeb.on("guildMemberAdd", async member => {
 naqeb.on("message", zaid => {
   if (zaid.content === prefix + "bot") {
     const bot = new Discord.RichEmbed()
-      .setAuthor()
+      
       .setTimestamp()
       .setFooter("Sponsored By: N1 SQUAD")
       .setColor("#FF0000")
