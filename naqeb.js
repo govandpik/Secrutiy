2a4c897e928c4f168b70dfe03555c7ba3d55e5f7
govandpik <69141721+govandpik@users.noreply.github.com>
2020-09-22 10:53:20 +0300

    Update naqeb.js

--- naqeb.js
+++ naqeb.js
@@ -731,9 +731,9 @@ naqeb.on("message", message => {
 naqeb.on('message', message => {
  
 if(message.content.includes("@everyone")){
-if(!message.member.hasPermission('KICK_MEMBERS')){
+if(!message.member.hasPermission('MEMBER_KICK')){
 message.delete(); 
-message.reply("tkaya everyoune lemada kick dakrey")
+message.reply(", you cant send everyone message")
 }
  
 }
@@ -744,14 +744,38 @@ message.reply("tkaya everyoune lemada kick dakrey")
 naqeb.on('message', message => {
  
 if(message.content.includes("@everyone@everyone")){
-if(!message.member.hasPermission('KICK_MEMBERS')){
+if(!message.member.hasPermission('MEMBER_KICK')){
 message.delete(); 
-message.reply("tkaya everyone lemada kick dakreyt")
+message.reply(", you cant send everyone message")
 }
  
 }
  
 });
+
+
+naqeb.on('message', message => {
+     if(!message.channel.guild) return;
+                if(message.content == prefix + 'allbots') {
+
+
+    if (message.author.bot) return;
+    let i = 1;
+        const botssize = message.guild.members.filter(m=>m.user.bot).map(m=>`${i++} - <@${m.id}>`);
+          const embed = new Discord.RichEmbed()
+          .setAuthor(message.author.tag, message.author.avatarURL)
+          .setDescription(`**Found ${message.guild.members.filter(m=>m.user.bot).size} bots in this Server**
+${botssize.join('\n')}`)
+.setFooter(client.user.username, client.user.avatarURL)
+.setTimestamp();
+message.channel.send(embed)
+
+}
+
+
+});
+
+
 
 
 naqeb.on("message", async message => {
